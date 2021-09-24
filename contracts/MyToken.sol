2be884daab6a92564738446443c446a1f4691c69
Abdul Maajid <53260291+abdul-maajid@users.noreply.github.com>
2021-09-24 13:40:25 +0500

    Add files via upload

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,1 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+
+contract BLSToken is ERC20 {
+	
+	constructor() public ERC20("BlocTech", "BLS") {
+		_mint(msg.sender, 18);
+	}
+}
